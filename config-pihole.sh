08575f2b8dcc0ee5040c5738c998dec7a319f663
Matze <66997348+MatzeNbg@users.noreply.github.com>
2020-06-29 15:59:10 +0200

    Create config-pihole.sh

---
+++ config-pihole.sh
@@ -0,0 +1,6 @@
+wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-arm.tgz
+tar -xvzf cloudflared-stable-linux-arm.tgz
+sudo cp ./cloudflared /usr/local/bin
+sudo chmod +x /usr/local/bin/cloudflared
+cloudflared -v
+sudo useradd -s /usr/sbin/nologin -r -M cloudflared
