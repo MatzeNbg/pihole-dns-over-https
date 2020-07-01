388589bcb445021b4001e2996e3da10fc0999b04
Matze <66997348+MatzeNbg@users.noreply.github.com>
2020-07-01 08:37:33 +0200

    Create secure-dns-cloudflare-setup.sh

---
+++ secure-dns-cloudflare-setup.sh
@@ -0,0 +1,5 @@
+wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-arm.tgz
+tar -xvzf cloudflared-stable-linux-arm.tgz
+sudo cp ./cloudflared /usr/local/bin
+sudo chmod +x /usr/local/bin/cloudflared
+cloudflared -v
